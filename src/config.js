5712e0c853c17a1be1e0adf0c72c47c0fac49f05
Eduardo Bruno <eduardo.luis.bruno@gmail.com>
2021-11-11 17:21:33 -0300

    First commit

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export default {
+  api: {
+    baseUrl: 'https://api.spotify.com/v1',
+    authUrl: 'https://accounts.spotify.com/api/token',
+    clientId: '48f495292ea6400e80355d7d2b1b137b',
+    clientSecret: '5c994b0eb0cc4dcd998f699838404d9e'
+  }
+}
