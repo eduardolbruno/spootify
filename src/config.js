0d6a26a479bea1f0c53be06b6aa3ec7f290f1b4f
Eduardo <eduardo.luis.bruno@gmail.com>
2021-11-17 13:07:08 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export default {
   api: {
     baseUrl: 'https://api.spotify.com/v1',
     authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: '48f495292ea6400e80355d7d2b1b137b',
-    clientSecret: '5c994b0eb0cc4dcd998f699838404d9e'
+    clientId: '',
+    clientSecret: ''
   }
 }
